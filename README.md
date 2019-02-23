21a81f8cca1e241b71bc748069ad28ea2e7a661d
tusiel <simondrake1990+tusielGithub@gmail.com>
2019-02-23 12:25:49 +0000

    Basic project structure and first test

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# payments-api
-Tech tech for Form3
+# Payments API
+
+## Summary
+This project was created as a technical exercise for Form3. 
+
+## Project Design
+An overall architectural design can be found [here](https://github.com/tusiel/payments-api/blob/master/design/ArchitecturalDiagram.pdf) which shows how the Payments Manager microservice could be implemented from an infrastructure point of view. 
+
+A flow diagram of how the Payments Manager microservice can be found [here](https://github.com/tusiel/payments-api/blob/master/design/flowDiagram.pdf). It also outlines the specified endpoints the application will expose. 
+
+## Running & building the application
+Two scripts have been created to make the process of running this application easier. 
+
+- `run.sh` will run the application with the `--race` flag. This is intended to be for development purposes. 
+- `build.sh` will create a `build` folder, which will have `darwin` and `linux` compiled versions of the application. This is intended to be for production purposes. 
+
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	go func() {
+		<-signalChan
+
+		appCleanup()
+		os.Exit(1)
+	}()
+
+	start()
+}
+
+func appCleanup() {
+	log.Println("Shutting down server...")
+}
+
+func start() {
+	router := mux.NewRouter()
+
+	/** API Routes */
+	api := router.PathPrefix("/api/v1").Subrouter()
+
+	api.HandleFunc("/payments", simpleHandler).Methods("GET")
+	api.HandleFunc("/payments/{id}", simpleHandler).Methods("GET")
+	api.HandleFunc("/payment", simpleHandler).Methods("POST")
+	api.HandleFunc("/payment/{id}", simpleHandler).Methods("PUT")
+	api.HandleFunc("/payment/{id}", simpleHandler).Methods("DELETE")
+
+	var handler http.Handler
+	handler = router
+	handler = handlers.LoggingHandler(os.Stdout, handler)
+
+	srv := &http.Server{
+		Handler:      handler,
+		Addr:         "localhost:3005",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func simpleHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte("Hello World"))
+}
