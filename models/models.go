2ad235286d7d18991c2ebb391b437c5524dea6ad
tusiel <simondrake1990+tusielGithub@gmail.com>
2019-02-23 14:05:50 +0000

    Add Payments model

---
+++ models/models.go
@@ -0,0 +1,83 @@
+package models
+
+// Payment is the overall payment model
+type Payment struct {
+	Type           string     `json:"type,omitempty" bson:"type,omitempty"`
+	ID             string     `json:"_id,omitempty" bson:"_id,omitempty"`
+	Version        uint16     `json:"version" bson:"version"`
+	OrganisationID string     `json:"organisation_id,omitempty" bson:"organisation_id,omitempty"`
+	Attributes     Attributes `json:"attributes,omitempty" bson:"attributes,omitempty"`
+}
+
+// Attributes is the Attributes model
+type Attributes struct {
+	Amount               string             `json:"amount,omitempty" bson:"amount,omitempty"`
+	BeneficiaryParty     BeneficiaryParty   `json:"beneficiary_party,omitempty" bson:"beneficiary_party,omitempty"`
+	ChargesInformation   ChargesInformation `json:"charges_information,omitempty" bson:"charges_information,omitempty"`
+	Currency             string             `json:"currency,omitempty" bson:"currency,omitempty"`
+	DebtorParty          DebtorParty        `json:"debtor_party,omitempty" bson:"debtor_party,omitempty"`
+	EndToEndReference    string             `json:"end_to_end_reference,omitempty" bson:"end_to_end_reference,omitempty"`
+	FX                   FX                 `json:"fx,omitempty" bson:"fx,omitempty"`
+	NumericReference     string             `json:"numeric_reference,omitempty" bson:"numeric_reference,omitempty"`
+	PaymentID            string             `json:"payment_id,omitempty" bson:"payment_id,omitempty"`
+	PaymentPurpose       string             `json:"payment_purpose,omitempty" bson:"payment_purpose,omitempty"`
+	PaymentScheme        string             `json:"payment_scheme,omitempty" bson:"payment_scheme,omitempty"`
+	PaymentType          string             `json:"payment_type,omitempty" bson:"payment_type,omitempty"`
+	ProcessingDate       string             `json:"processing_date,omitempty" bson:"processing_date,omitempty"`
+	Reference            string             `json:"reference,omitempty" bson:"reference,omitempty"`
+	SchemePaymentSubType string             `json:"scheme_payment_sub_type,omitempty" bson:"scheme_payment_sub_type,omitempty"`
+	SchemePaymentType    string             `json:"scheme_payment_type,omitempty" bson:"scheme_payment_type,omitempty"`
+	SponsorParty         SponsorParty       `json:"sponsor_party,omitempty" bson:"sponsor_party,omitempty"`
+}
+
+// BeneficiaryParty is the BeneficiaryParty model
+type BeneficiaryParty struct {
+	AccountName       string `json:"account_name,omitempty" bson:"account_name,omitempty"`
+	AccountNumber     string `json:"account_number,omitempty" bson:"account_number,omitempty"`
+	AccountNumberCode string `json:"account_number_code,omitempty" bson:"account_number_code,omitempty"`
+	AccountType       uint16 `json:"account_type" bson:"account_type"`
+	Address           string `json:"address,omitempty" bson:"address,omitempty"`
+	BankID            string `json:"bank_id,omitempty" bson:"bank_id,omitempty"`
+	BankIDCode        string `json:"bank_id_code,omitempty" bson:"bank_id_code,omitempty"`
+	Name              string `json:"name,omitempty" bson:"name,omitempty"`
+}
+
+// ChargesInformation is the ChargesInformation model
+type ChargesInformation struct {
+	BearerCode              string         `json:"bearer_code,omitempty" bson:"bearer_code,omitempty"`
+	SenderCharges           []SenderCharge `json:"sender_charges,omitempty" bson:"sender_charges,omitempty"`
+	ReceiverChargesAmount   string         `json:"receiver_charges_amount,omitempty" bson:"receiver_charges_amount,omitempty"`
+	ReceiverChargesCurrency string         `json:"receiver_charges_currency,omitempty" bson:"receiver_charges_currency,omitempty"`
+}
+
+// SenderCharge is the SenderCharge model
+type SenderCharge struct {
+	Amount   string `json:"amount,omitempty" bson:"amount,omitempty"`
+	Currency string `json:"currency,omitempty" bson:"currency,omitempty"`
+}
+
+// DebtorParty is the DebtorParty model
+type DebtorParty struct {
+	AccountName       string `json:"account_name,omitempty" bson:"account_name,omitempty"`
+	AccountNumber     string `json:"account_number,omitempty" bson:"account_number,omitempty"`
+	AccountNumberCode string `json:"account_number_code,omitempty" bson:"account_number_code,omitempty"`
+	Address           string `json:"address,omitempty" bson:"address,omitempty"`
+	BankID            string `json:"bank_id,omitempty" bson:"bank_id,omitempty"`
+	BankIDCode        string `json:"bank_id_code,omitempty" bson:"bank_id_code,omitempty"`
+	Name              string `json:"name,omitempty" bson:"name,omitempty"`
+}
+
+// FX is the FX model
+type FX struct {
+	ContractReference string `json:"contract_reference,omitempty" bson:"contract_reference,omitempty"`
+	ExchangeRate      string `json:"exchange_rate,omitempty" bson:"exchange_rate,omitempty"`
+	OriginalAmount    string `json:"original_amount,omitempty" bson:"original_amount,omitempty"`
+	OriginalCurrency  string `json:"original_currency,omitempty" bson:"original_currency,omitempty"`
+}
+
+// SponsorParty is the SponsorParty model
+type SponsorParty struct {
+	AccountNumber string `json:"account_number,omitempty" bson:"end_to_end_reference,omitempty"`
+	BankID        string `json:"bank_id,omitempty" bson:"bank_id,omitempty"`
+	BankIDCode    string `json:"bank_id_code,omitempty" bson:"bank_id_code,omitempty"`
+}
---
+++ models/models_test.go
@@ -0,0 +1,76 @@
+package models
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	expected := `{"type":"Payment","_id":"6cd862ab-6d40-4a86-8037-77d446b3f6fc","version":0,"organisation_id":"743d5b63-8e6f-432e-a8fa-c5d8d2ee5fcb","attributes":{"amount":"100.21","beneficiary_party":{"account_name":"W Owens","account_number":"31926819","account_number_code":"BBAN","account_type":0,"address":"1 The Beneficiary Localtown SE2","bank_id":"403000","bank_id_code":"GBDSC","name":"Wilfred Jeremiah Owens"},"charges_information":{"bearer_code":"SHAR","sender_charges":[{"amount":"5.00","currency":"GBP"},{"amount":"10.00","currency":"USD"}],"receiver_charges_amount":"1.00","receiver_charges_currency":"USD"},"currency":"GBP","debtor_party":{"account_name":"EJ Brown Black","account_number":"GB29XABC10161234567801","account_number_code":"IBAN","address":"10 Debtor Crescent Sourcetown NE1","bank_id":"203301","bank_id_code":"GBDSC","name":"Emelia Jane Brown"},"end_to_end_reference":"Wil piano Jan","fx":{"contract_reference":"FX123","exchange_rate":"2.00000","original_amount":"200.42","original_currency":"USD"},"numeric_reference":"1002001","payment_id":"123456789012345678","payment_purpose":"Paying for goods/services","payment_scheme":"FPS","payment_type":"Credit","processing_date":"2017-01-18","reference":"Payment for Em's piano lessons","scheme_payment_sub_type":"InternetBanking","scheme_payment_type":"ImmediatePayment","sponsor_party":{"account_number":"56781234","bank_id":"123123","bank_id_code":"GBDSC"}}}`
+
+	payment := Payment{
+		Type:           "Payment",
+		ID:             "6cd862ab-6d40-4a86-8037-77d446b3f6fc",
+		Version:        0,
+		OrganisationID: "743d5b63-8e6f-432e-a8fa-c5d8d2ee5fcb",
+		Attributes: Attributes{
+			Amount: "100.21",
+			BeneficiaryParty: BeneficiaryParty{
+				AccountName:       "W Owens",
+				AccountNumber:     "31926819",
+				AccountNumberCode: "BBAN",
+				AccountType:       0,
+				Address:           "1 The Beneficiary Localtown SE2",
+				BankID:            "403000",
+				BankIDCode:        "GBDSC",
+				Name:              "Wilfred Jeremiah Owens",
+			},
+			ChargesInformation: ChargesInformation{
+				BearerCode: "SHAR",
+				SenderCharges: []SenderCharge{
+					{Amount: "5.00", Currency: "GBP"},
+					{Amount: "10.00", Currency: "USD"},
+				},
+				ReceiverChargesAmount:   "1.00",
+				ReceiverChargesCurrency: "USD",
+			},
+			Currency: "GBP",
+			DebtorParty: DebtorParty{
+				AccountName:       "EJ Brown Black",
+				AccountNumber:     "GB29XABC10161234567801",
+				AccountNumberCode: "IBAN",
+				Address:           "10 Debtor Crescent Sourcetown NE1",
+				BankID:            "203301",
+				BankIDCode:        "GBDSC",
+				Name:              "Emelia Jane Brown",
+			},
+			EndToEndReference: "Wil piano Jan",
+			FX: FX{
+				ContractReference: "FX123",
+				ExchangeRate:      "2.00000",
+				OriginalAmount:    "200.42",
+				OriginalCurrency:  "USD",
+			},
+			NumericReference:     "1002001",
+			PaymentID:            "123456789012345678",
+			PaymentPurpose:       "Paying for goods/services",
+			PaymentScheme:        "FPS",
+			PaymentType:          "Credit",
+			ProcessingDate:       "2017-01-18",
+			Reference:            "Payment for Em's piano lessons",
+			SchemePaymentSubType: "InternetBanking",
+			SchemePaymentType:    "ImmediatePayment",
+			SponsorParty: SponsorParty{
+				AccountNumber: "56781234",
+				BankID:        "123123",
+				BankIDCode:    "GBDSC",
+			},
+		},
+	}
+
+	r, _ := json.Marshal(payment)
+
+	if string(r) != expected {
+		t.Error("Marshalled Payment object does not match what was expected")
+	}
+}
